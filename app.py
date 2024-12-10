0a86bb5cb1f67fa6728a3c0745aeb76bf88b49f5
Harriet Hall <harriet.hall@madetech.com>
2024-12-10 13:45:02 +0000

    test get courses data

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def index():
     return 'hello, World!'
 
+@app.route('/courses')
+def courses():
+  return "<h1>Courses</h1><ul><li>Course 1</li><li>Course 2</li><li>Course 3</li><li>Course 4</li></ul>"
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,13 @@
-import json
 def test_index(client):
     res = client.get('/')
     assert res.status_code == 200
+    
+def test_courses(client):
+    res = client.get('/courses')
+    assert res.status_code == 200
+    assert b"<h1>Courses</h1>" in res.data
+    assert b"<li>Course 1</li>" in res.data
+    assert b"<li>Course 2</li>" in res.data
+    assert b"<li>Course 3</li>" in res.data
+    assert b"<li>Course 4</li>" in res.data
  
